79a0a85287ccccde503c49ed5b7a428a5e146d31
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-02-02 12:59:54 -0800

    Add URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject me.arrdem/cuddlefish "0.1.0-SNAPSHOT"
   :description "A cute little git wrapper."
-  :url "http://example.com/FIXME"
+  :url "http://github.com/arrdem/cuddlefish"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
