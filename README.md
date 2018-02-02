81998d9051b4c62b003227edc5e61a58831f5b83
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-02-02 12:30:05 -0800

    Initial state

---
+++ README.md
@@ -0,0 +1,13 @@
+# cuddlefish
+<img align="right" src="https://github.com/arrdem/cuddlefish/raw/master/etc/cuddlefish.jpg" width=300/>
+
+A cute little git wrapper intended to be a light-weight, less
+featureful alternative to
+[clj-jgit](https://github.com/clj-jgit/clj-jgit).
+
+## License
+
+Copyright © 2018 Reid "arrdem" McKenzie
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,32 @@
+(defproject me.arrdem/cuddlefish "0.1.0-SNAPSHOT"
+  :description "A cute little git wrapper."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  
+  :source-paths      ["src/main/clj"
+                      "src/main/cljc"]
+  :java-source-paths ["src/main/jvm"]
+  :resource-paths    ["src/main/resources"]
+
+  :profiles
+  {:test {:test-paths        ["src/test/clj"
+                              "src/test/cljc"]
+          :java-source-paths ["src/test/jvm"]
+          :resource-paths    ["src/test/resources"]}
+   :dev  {:source-paths      ["src/dev/clj"
+                              "src/dev/cljc"]
+          :java-source-paths ["src/dev/jvm"]
+          :resource-paths    ["src/dev/resources"]}}
+
+  :plugins [[me.arrdem/lein-git-version "2.0.3"]]
+
+  :git-version
+  {:status-to-version
+   (fn [{:keys [tag version ahead ahead? dirty?] :as git}]
+     (if (and tag (not ahead?) (not dirty?))
+       tag
+       (str tag
+            (when ahead? (str "." ahead))
+            (when dirty? "-SNAPSHOT"))))})
